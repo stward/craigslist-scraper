87e36338539612a949c6d1f80ea52b3e4fa617c1
terrell linnell <terrell.linnell@gmail.com>
2017-04-12 14:56:28 -0600

    still broken need to restart comp

---
+++ src/Containers/ResultsContainer.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import ResultsDisplay from '../Views/ResultsDisplay';
+
+
+class ResultsContainer extends Component {
+  render () {
+    return (
+      <div>
+        <ResultsDisplay />
+      </div>
+    )
+  }
+}
+
+export default ResultsContainer;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import './App.css';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import HomeContainer from './Containers/HomeContainer';
+import ResultsContainer from './Containers/ResultsContainer';
+import DetailsDisplay from './Views/DetailsDisplay';
 
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory} >
+    <Route path='/' component={App}>
+      <IndexRoute component={HomeContainer} />
+        <Route path='city' component={ResultsContainer}>
+          <Route path="detailId" component={DetailsDisplay} />
+        </Route>
+    </Route>
+  </Router>,
   document.getElementById('root')
 );
